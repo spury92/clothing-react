bbe6b031f97e37cd364772061a366686f36a9bfe
spury92 <bot1412@yandex.ru>
2020-01-02 21:52:15 +0300

    created react app; added homepage; added components: directory, menu-item; all with styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import HomePage from './pages/homepage/homepage.content'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
