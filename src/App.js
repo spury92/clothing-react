7f8a44187c80d136f914c57c71b7b61c9c323680
spury92 <bot1412@yandex.ru>
2020-01-03 09:58:38 +0300

    added header; logo with crown

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.content'
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.content';
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
