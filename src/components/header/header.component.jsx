8bdce37c0319e61689e09f3469aa73bab2435a1b
spury92 <bot1412@yandex.ru>
2020-01-04 15:17:01 +0300

    added cart-icon and dropdown window for cart; static for now

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
@@ -16,11 +18,13 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
