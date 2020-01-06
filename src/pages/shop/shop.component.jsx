bf7db7827f6cf49aac6bcff7e9f32dbdb59616f1
spury92 <bot1412@yandex.ru>
2020-01-06 09:12:14 +0300

    added redux-persist; moved shop data and directory items into their own folders; added collections-overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,14 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
-    render() {
-        const {collections} = this.state;
-        return (
-        <div className='shop-page'>
-            {
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
-            ))
-            }
-        </div>);
-    }
-}
+
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionOverview />
+    </div>
+);
+
+
 
 export default ShopPage;
