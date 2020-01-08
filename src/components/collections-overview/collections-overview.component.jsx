284fdd841e9a4598e970086043aa7edf5224ed99
spury92 <bot1412@yandex.ru>
2020-01-08 20:57:11 +0300

    moved collectionsOverview and CollectionsPage into their own container files

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from '../collection-preview/collection-preview.component';
+import CollectionsPreview from '../collection-preview/collection-preview.component';
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import './collections-overview.styles.scss';
 
@@ -10,7 +10,7 @@ const collectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
         {
             collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
+                <CollectionsPreview key={id} {...otherCollectionProps}/>
             ))
         }
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 
-import CollectionOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
+import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionFetching, selectIsCollectionLoaded } from "../../redux/shop/shop.selectors";
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
     componentDidMount () {
@@ -19,34 +14,25 @@ class ShopPage extends React.Component {
     };
     
     render () {
-        const { match, isCollectionFetching, isCollectionLoaded } = this.props;
+        const { match } = this.props;
         return (
         <div className='shop-page'>
             <Route
                 exact 
                 path={`${match.path}`}
-                render={(props) => (
-                    <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props}/>
-                )}
+                component={CollectionsOverviewContainer}
             />
             <Route 
                 exact 
                 path={`${match.path}/:collectionId`}
-                render={(props) => (
-                    <CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props}/>
-                )}
+                component={CollectionPageContainer}
             />
         </div>)
     };
 };
 
-const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching,
-    isCollectionLoaded: selectIsCollectionLoaded
-});
-
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
